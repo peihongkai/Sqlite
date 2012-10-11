b4b27d2bc8cd79c965da94420b7c99fd1c0863fd
裴洪凯 <peihongkai@peimatoMacBook-Air.local>
2012-10-11 19:31:21 +0800

    Initial Commit

---
+++ Sqlite/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sqlite
+//
+//  Created by 裴洪凯 on 12-10-11.
+//  Copyright (c) 2012年 裴洪凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
